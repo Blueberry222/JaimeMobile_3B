e9752b5b7cb5623aed62f75ec4eeee30da417a12
Augusto <augusto-vial@hotmail.com>
2023-08-28 22:35:28 -0300

    Aula 28/08

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -9,6 +9,44 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
+
+
+  public movimento = 
+    {
+      data: "",
+      descricao: "",
+      operacao: "",
+      valor: 0
+    };
+
+  public movimentos: Array<{ data: string; descricao: string; operacao: string; valor: number }> = [];
+
+  calcularSaldo(){
+    let saldoTotal = 0;
+    this.movimentos.forEach(item => {
+      if (item.operacao == "credito"){
+        saldoTotal += item.valor;
+      } else {
+        saldoTotal -= item.valor;
+      }
+      
+    })
+    return saldoTotal;
+  }
+
+  adicionar() {
+    this.movimentos.push({
+      data: this.movimento.data,
+      descricao: this.movimento.descricao,
+      operacao: this.movimento.operacao,
+      valor: this.movimento.valor
+    })
+    this.movimento.data = ""
+    this.movimento.descricao = ""
+    this.movimento.operacao = ""
+    this.movimento.valor = 0
+  }
+
   constructor() {}
 
   ngOnInit() {
